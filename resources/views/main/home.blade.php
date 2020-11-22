4207bc0bc5827019c1f14a6b62a19de08dc2b875
Abdul <abdulkader.appcom@gmail.com>
2020-11-22 17:48:04 +0400

    intial commit

---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="shortcut icon" href="img/fav.png">
+	<meta name="author" content="appcom">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta charset="UTF-8">
+
+	<title>Swoop | Car Wash App</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,600|Roboto:400,400i,500" rel="stylesheet">
+	<link rel="stylesheet" href="{{asset('public/web_assets/css/linearicons.css')}}">
+	<link rel="stylesheet" href="{{asset('public/web_assets/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" href="{{asset('public/web_assets/css/bootstrap.css')}}">
+	<link rel="stylesheet" href="{{asset('public/web_assets/css/magnific-popup.css')}}">
+	<link rel="stylesheet" href="{{asset('public/web_assets/css/nice-select.css')}}">
+	<link rel="stylesheet" href="{{asset('public/web_assets/css/hexagons.min.css')}}">
+	<link rel="stylesheet" href="{{asset('public/web_assets/css/owl.carousel.css')}}">
+	<link rel="stylesheet" href="{{asset('public/web_assets/css/main.css')}}">
+</head>
+
+<body>
+
+		<!-- start banner Area -->
+		<section class="home-banner-area">
+			<div class="container">
+				<div class="row fullscreen d-flex align-items-center justify-content-between">
+					<div class="home-banner-content col-lg-6 col-md-6">
+	                    <div id="logo" style="margin-bottom: 2rem;">
+						   <a href="#"><img src="{{asset('public/web_assets/img/logo.png')}}" alt="Swoop Logo" height="60px" width="auto" title="Swoop Car Wash Logo" /></a>
+					    </div>
+						<h1>Keeping <br> that car clean!</h1>
+						<p>Convenient cost-effective car washing solution</p>
+						<div class="download-button d-flex flex-row justify-content-start">
+							<div class="flex-row d-flex">
+								<a href="#"><img src="{{asset('public/web_assets/img/app-store1.jpg')}}" alt="download on appstore" width="180px" /></a>
+							</div>
+							<div class="flex-row d-flex">
+								<a href="https://play.google.com/store/apps/details?id=com.appcom.app.swoopthecarwash"><img src="{{asset('public/web_assets/img/app-store2.jpg')}}" alt="download on play store" width="180px" /></a>
+							</div>
+						</div>
+					</div>
+					<div class="banner-img col-lg-4 col-md-6" style="text-align: center;">
+						<img class="img-fluid" src="{{asset('public/web_assets/img/banner-img.png')}}" id="lightmode" alt="Swoop App Light">
+	                    <div>
+	                        <button type="button" class="mt-3 btn btn-sm btn-secondary" onclick="changeImage()">Switch Mode</button>
+	                    </div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End banner Area -->
+
+		<!-- Start feature Area -->
+		<section class="feature-area">
+			<div class="container">
+				<div class="row d-flex justify-content-center">
+					<div class="col-lg-6">
+						<div class="section-title text-center">
+							<h2>Looking to get your car cleaned?</h2>
+							<p>Don’t wait, download Swoop Car Wash now and put your feet up while our professional cleaners take care of your car.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-md-6">
+						<div class="single-feature">
+							<a href="#" class="title">
+								<span class="lnr lnr-users"></span>
+								<h3>Expert Cleaners</h3>
+							</a>
+							<p>
+								Our expert cleaners are trained to keep your car clean and clear.
+							</p>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6">
+						<div class="single-feature">
+							<a href="#" class="title">
+								<span class="lnr lnr-calendar-full"></span>
+								<h3>Monthly Plans</h3>
+							</a>
+							<p>
+								We have flexible plans, So you dont have to worry about your budget.
+							</p>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-6">
+						<div class="single-feature">
+							<a href="#" class="title">
+								<span class="lnr lnr-car"></span>
+								<h3>Washes &amp; Schedules</h3>
+							</a>
+							<p>
+								Your can also view the schedule for your car wash from the app.
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!-- End feature Area -->
+
+		<!-- Start about Area -->
+		<section class="about-area">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-5 home-about-left">
+						<img class="img-fluid" src="{{asset('public/web_assets/img/iphone.png')}}" alt="">
+					</div>
+					<div class="offset-lg-1 col-lg-5 home-about-right">
+						<h1>
+							Log in and <br>
+							we get  Washing
+						</h1>
+						<p>
+							One App to control it all! Manage all your cars and payments via the designated app. Its simple fast and efficient just add your car and it gets cleaned. Magic!
+						</p>
+					</div>
+					<div class="col-lg-6 home-about-right home-about-right2">
+						<h1>
+							Keep Track <br>
+							and in control
+						</h1>
+						<p>
+	                        Our designated app keeps customers informed when washes are due and once they are completed. You’ll know exactly how many washes you have got and how many you are yet to receive.</p><p>Manage your wash requirements and subscriptions or even give special instructions to the staff right from your phone.
+						</p>
+					</div>
+					<div class="col-lg-5 home-about-left">
+						<img class="img-fluid" src="{{asset('public/web_assets/img/iphone1.png')}}" alt="">
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End about Area -->
+
+	    <section>
+	        <div class="container">
+	            <div class="row mb-5">
+	                <div class="col py-5">
+	                    <h1 class="text-primary text-center">Let’s improve the building car washing procedure together.</h1>
+	                    <h1 style="font-size: 3.5rem;text-align: center;">Get Swoop and let us get cleaning.</h1>
+	                </div>
+	            </div>
+	        </div>
+	    </section>
+
+		<!-- Start Footer Area -->
+		<footer class="footer-area section-gap" style="background: #000 !important;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-md-12 single-footer-widget">
+						<a href=""><img src="{{asset('public/web_assets/img/logo.png')}}" width="auto" height="72" class="mb-3" alt="Swoop Car Wash Logo" /></a>
+	                    <ul>
+							<li><a href="">Terms &amp; Conditions</a></li>
+	              <li><a href="">Privacy Policy</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6 single-footer-widget">
+						<h4>Contact Us</h4>
+						<ul>
+							<li><i class="fa fa-phone"></i> <a href="tel:+9714515253"> +971 451 5253</a></li>
+							<li><i class="fa fa-envelope"></i> <a href="mailto:support@swoopcarwash.com">support@swoopcarwash.com</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6 single-footer-widget">
+						<h4>Social Media</h4>
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i> Facebook</a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i> Twitter</a></li>
+							<li><a href="#"><i class="fa fa-instagram"></i> Instagram</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer-bottom row align-items-center">
+					<p class="footer-text m-0 col-lg-6 col-md-12">
+	                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Swoop Parking Car Washing L.L.C</p>
+					<div class="col-lg-6 col-md-6 social-link">
+						<div class="download-button d-flex flex-row justify-content-end">
+							<div class="buttons rounded gray flex-row d-flex">
+								<i class="fa fa-apple" aria-hidden="true"></i>
+								<div class="desc">
+									<a href="#">
+										<p>
+											<span>Available</span> <br>
+											on App Store
+										</p>
+									</a>
+								</div>
+							</div>
+							<div class="buttons rounded gray flex-row d-flex">
+								<i class="fa fa-android" aria-hidden="true"></i>
+								<div class="desc">
+									<a href="#">
+										<p>
+											<span>Available</span> <br>
+											on Play Store
+										</p>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- End Footer Area -->
+    <script>
+        function changeImage() {
+            var image = document.getElementById('lightmode');
+            if (image.src.match("{{asset('public/web_assets/img/banner-img-dark.png')}}")) {
+                image.src = "{{asset('public/web_assets/img/banner-img.png')}}";
+            }
+            else {
+                image.src = "{{asset('public/web_assets/img/banner-img-dark.png')}}";
+            }
+        }
+    </script>
+		<script src="{{asset('public/web_assets/js/vendor/jquery-2.2.4.min.js')}}"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+		<script src="{{asset('public/web_assets/js/tilt.jquery.min.js')}}"></script>
+		<script src="{{asset('public/web_assets/js/vendor/bootstrap.min.js')}}"></script>
+		<script src="{{asset('public/web_assets/js/easing.min.js')}}"></script>
+		<script src="{{asset('public/web_assets/js/owl.carousel.min.js')}}"></script>
+		<script src="{{asset('public/web_assets/js/owl-carousel-thumb.min.js')}}"></script>
+		<script src="{{asset('public/web_assets/js/main.js')}}"></script>
+</body>
+
+</html>
